a15a9fb8dc6d7fb452c86f66d6471511ea97eb93
celineblle <celineblle@outlook.fr>
2023-11-02 09:44:23 +0100

    commit final

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import "./App.css";
+import mountain from "./assets/mountain.jpg";
+import star from "./assets/favorite.png";
+import halfStar from "./assets/rating.png";
+
+const itemTab = [
+  {
+    id: "1",
+    name: "Item 1",
+  },
+  {
+    id: "2",
+    name: "Item 2",
+  },
+  {
+    id: "3",
+    name: "Item 3",
+  },
+  {
+    id: "4",
+    name: "Item 4",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="global">
+      <img src={mountain} />
+      <div className="subTexte">
+        <div className="entete">
+          <h1>Title</h1>
+          <div className="noteAndStars">
+            <img className="stars" src={star} />
+            <img className="stars" src={star} />
+            <img className="stars" src={star} />
+            <img className="stars" src={star} />
+            <img className="stars" src={halfStar} />
+            <p className="note">4(413)</p>
+          </div>
+        </div>
+        <p className="titleDescription">
+          Secondary test. <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
+        <div className="endElement">
+          <h2>Subtitle</h2>
+          {itemTab.map((element, index) => {
+            return <button key={index}>{element.name}</button>;
+          })}
+        </div>
+        <p className="lien">ACTION 1</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
